5d82555c7dcd02c13168d55765abae0e7bc047f2
Philipp Sackl <philipp@mozilla.com>
2014-03-12 14:57:47 +0100

    add support for left mouse button

--- Gmynd.Mouse.js
+++ Gmynd.Mouse.js
@@ -4,7 +4,10 @@
     position: {
       x: 0,
       y: 0
-    }
+    },
+    leftButtonDown: false,
+    rightButtonDown: false,
+    middleButtonDown: false
   }
 
   // Update the mouse position whenever the mouse is being moved
@@ -12,4 +15,11 @@
     G.Mouse.position.x = e.clientX;
     G.Mouse.position.y = e.clientY;
   });
+  // Global click handlers
+  document.addEventListener('mousedown', function(e) {
+    G.Mouse.leftButtonDown = true;
+  });
+  document.addEventListener('mouseup', function(e) {
+    G.Mouse.leftButtonDown = false;
+  })
 })();
